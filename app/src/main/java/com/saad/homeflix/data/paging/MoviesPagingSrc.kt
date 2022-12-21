4323d0644c639fa36347c38114d73b236c5f05f8
saad35201 <saadasad35201@gmail.com>
2022-12-21 09:50:21 -0800

    working on pagination

--- app/src/main/java/com/saad/homeflix/data/paging/MoviesPagingSrc.kt
+++ app/src/main/java/com/saad/homeflix/data/paging/MoviesPagingSrc.kt
@@ -4,17 +4,19 @@ import androidx.paging.PagingSource
 import androidx.paging.PagingState
 import com.saad.homeflix.data.models.ResultsItem
 import com.saad.homeflix.data.network.remote.MoviesApiService
-import com.saad.homeflix.utils.API_KEY
+import com.saad.homeflix.utils.Constants
+import com.saad.homeflix.utils.Constants.API_KEY
 
 class MoviesPagingSrc(private val service: MoviesApiService) : PagingSource<Int, ResultsItem>() {
 
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, ResultsItem> {
         return try {
-            val currentPage = params.key ?: 1
+            val currentPage = params.key ?: Constants.NETWORK_PAGE_NUMBER
             val response = service.getMovies(API_KEY, currentPage)
+            val movies = response.body()!!.results!!
 
             LoadResult.Page(
-                data = response.body()!!.results!!,
+                data = movies,
                 prevKey = if (currentPage == 1) null else currentPage - 1,
                 nextKey = if (currentPage == response.body()!!.totalPages) null else currentPage + 1
             )
--- app/src/main/java/com/saad/homeflix/utils/Constants.kt
+++ app/src/main/java/com/saad/homeflix/utils/Constants.kt
@@ -1,9 +1,14 @@
 package com.saad.homeflix.utils
 
-const val TAG = "Homeflix"
-const val SPLASH_DELAY = 3000
-const val BASE_URL = "https://api.themoviedb.org/3/"
-const val LOGIN_URL = "https://notes-api-sample.herokuapp.com/users/signin"
-const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w92/"
-const val API_KEY = "e5ea3092880f4f3c25fbc537e9b37dc1"
-const val LOCAL_DATABASE_NAME = "movies_database"
+object Constants {
+    const val TAG = "Homeflix"
+    const val SPLASH_DELAY = 3000
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val LOGIN_URL = "https://notes-api-sample.herokuapp.com/users/signin"
+    const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w92/"
+    const val API_KEY = "e5ea3092880f4f3c25fbc537e9b37dc1"
+    const val LOCAL_DATABASE_NAME = "movies_database"
+
+    const val NETWORK_PAGE_NUMBER = 1
+    const val NETWORK_PAGE_SIZE = 20
+}
